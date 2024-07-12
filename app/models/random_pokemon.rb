9b3bc8eca1e21bb6f19698187195b31018d9c62d
Chris van der Meer <cvandermeer@live.nl>
2024-07-12 11:22:49 +0200

    added a basic stat value calculator

--- app/models/random_pokemon.rb
+++ app/models/random_pokemon.rb
@@ -12,21 +12,29 @@ class RandomPokemon
       sample_base_pokemon.pokemons.create(
         trainer: nil,
         level: level,
-        hp: 1,
-        attack: 1,
-        special_attack: 1,
-        defense: 1,
-        special_defense: 1,
-        speed: 1,
-        current_hp: 1,
-        current_attack: 1,
-        current_special_attack: 1,
-        current_defense: 1,
-        current_special_defense: 1,
-        current_speed: 1,
+        hp: stat_value(sample_base_pokemon.hp, level),
+        attack: stat_value(sample_base_pokemon.attack, level),
+        special_attack: stat_value(sample_base_pokemon.special_attack, level),
+        defense: stat_value(sample_base_pokemon.defense, level),
+        special_defense: stat_value(sample_base_pokemon.special_defense, level),
+        speed: stat_value(sample_base_pokemon.speed, level),
+        current_hp: stat_value(sample_base_pokemon.hp, level),
+        current_attack: stat_value(sample_base_pokemon.attack, level),
+        current_special_attack: stat_value(sample_base_pokemon.special_attack, level),
+        current_defense: stat_value(sample_base_pokemon.defense, level),
+        current_special_defense: stat_value(sample_base_pokemon.special_defense, level),
+        current_speed: stat_value(sample_base_pokemon.speed, level),
         current_experience: 0,
         experience_to_level: 0
       )
+    end
+
+    private
+
+    def stat_value(base_stat, level)
+      return 1 unless base_stat
+
+      base_stat + (base_stat / 50) * level
     end
   end
 end
--- spec/models/random_pokemon_spec.rb
+++ spec/models/random_pokemon_spec.rb
@@ -14,5 +14,13 @@ RSpec.describe RandomPokemon, type: :model do
       expect(RandomPokemon.retrieve(ash).level)
         .to be_within(3).of(ash.pokemons.maximum(:level))
     end
+
+    it 'calulates the stats of the pokemon' do
+      random_pokemon = RandomPokemon.retrieve
+
+      expect(random_pokemon.attack).to be_within(5).of(
+        RandomPokemon.send(:stat_value, random_pokemon.base_pokemon.attack, random_pokemon.level)
+      )
+    end
   end
 end
